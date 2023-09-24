1821fdcb7e22edb9b247fdc385f353050a536af2
enmarcm <theenmanuel123@gmail.com>
2023-09-24 01:35:27 -0400

    Uso de spread operator para objetos y cambios de hover en el css

--- 00-hola-mundo/src/App.jsx
+++ 00-hola-mundo/src/App.jsx
@@ -2,12 +2,15 @@ import TwitterCard from "./TwitterCard.jsx";
 export const App = () => {
   const setArroba = (userName) => `@${userName}`;
 
+  const enmarcm = { userName: "enmarcm", isFollowing: true };
+  const midudev = { userName: "midudev", isFollowing: false };
+
   return (
     <div className="app">
-      <TwitterCard formatUser={setArroba} userName="midudev" isFollowing={true}>
+      <TwitterCard formatUser={setArroba} {...midudev}>
         Miguel Angel Duran
       </TwitterCard>
-      <TwitterCard formatUser={setArroba} userName="enmarcm" isFollowing={true}>
+      <TwitterCard formatUser={setArroba} {...enmarcm}>
         Enmanuel Ramon Colina
       </TwitterCard>
     </div>
