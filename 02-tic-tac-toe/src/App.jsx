db2a337d284ed76046c372852bfbfa23a3e5c209
enmarcm <theenmanuel123@gmail.com>
2023-10-08 23:20:52 -0400

    Culminacion del Tictactoe, se implemento mensaje del ganador y empate, ademas de agregar confetti con canvas-confetti

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Square from "./Square.jsx";
+import confetti from "canvas-confetti";
 
 const WINNER_COMBOS = [
   [0, 1, 2], // Horizontal
@@ -9,8 +10,8 @@ const WINNER_COMBOS = [
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8], // Diagonal
-  [2, 4, 6]  
-]
+  [2, 4, 6],
+];
 
 function App() {
   const TURNS = {
@@ -22,21 +23,29 @@ function App() {
   const [turn, setTurn] = useState(TURNS.X);
   const [winner, setWinner] = useState(null); // Null es que no hay ganador, false es que hay empate
 
-  const checkWinner = (boardToCheck) =>{
+  const checkWinner = (boardToCheck) => {
     for (const combo of WINNER_COMBOS) {
-      const [a,b,c] = combo
+      const [a, b, c] = combo;
 
-      if(boardToCheck[a] && boardToCheck[a]===boardToCheck[b] && boardToCheck[a]===boardToCheck[c]){
-        return boardToCheck[a]
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
       }
- 
     }
     //Aun no hay ganador
-    return null
+    return null;
+  };
+
+  const checkEndGame = (boardToCheck) => {
+    //Aqui lo que vamos a hacer es revisar si tiene espacios vacios, si no tiene y aun no hay ganador entonces es empate
+    return boardToCheck.every(element=>element!==null)
   }
- 
-  const updateBoard = (index) => { 
-    if(board[index] || winner) return 
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return;
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
@@ -44,14 +53,21 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
 
-    //Verificamos si hay ganador 
-    const newWinner = checkWinner(newBoard)
-    console.log(newBoard)
+    //Verificamos si hay ganador
+    const newWinner = checkWinner(newBoard);
 
-    if(newWinner) {
-      setWinner(newWinner)
-      alert(`Ganó ${newWinner}`)
+    if (newWinner) {
+      setWinner(newWinner);
+      confetti();
+    }else if(checkEndGame(newBoard)){
+      setWinner(false);
     }
+  };
+
+  const reiniciar = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
   };
 
   return (
@@ -71,6 +87,15 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      {winner !== null && (
+        <section className="winner">
+          <h2>
+            {winner === false ? "Empate" : `Ganó ${winner}`}
+            <button onClick={reiniciar}>Reiniciar</button>
+          </h2>
+        </section>
+      )}
     </main>
   );
 }
