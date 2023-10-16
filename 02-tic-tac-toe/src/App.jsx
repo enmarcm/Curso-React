7a6388e9aad776cc694c14fdb65b1c0a9ab19a57
enmarcm <theenmanuel123@gmail.com>
2023-10-15 23:39:11 -0400

    Local Storage y componentizacion

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,48 +1,21 @@
 import { useState } from "react";
-import Square from "./Square.jsx";
+import Square from "./components/Square.jsx";
 import confetti from "canvas-confetti";
-
-const WINNER_COMBOS = [
-  [0, 1, 2], // Horizontal
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6], // Vertical
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8], // Diagonal
-  [2, 4, 6],
-];
+import { TURNS } from "./constants.js";
+import { checkWinner, checkEndGame } from "./logic/index.js";
+import { saveGameToStorage, resetGameStorage } from "./logic/storage.js";
+import Winner from "./components/Winner.jsx";
 
 function App() {
-  const TURNS = {
-    X: "x",
-    O: "o",
-  };
-
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const localBoard = window.localStorage.getItem("board");
+    return localBoard ? JSON.parse(localBoard) : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const localTurn = window.localStorage.getItem("turn");
+    return localTurn ? localTurn : TURNS.X;
+  });
   const [winner, setWinner] = useState(null); // Null es que no hay ganador, false es que hay empate
-
-  const checkWinner = (boardToCheck) => {
-    for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo;
-
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a];
-      }
-    }
-    //Aun no hay ganador
-    return null;
-  };
-
-  const checkEndGame = (boardToCheck) => {
-    //Aqui lo que vamos a hacer es revisar si tiene espacios vacios, si no tiene y aun no hay ganador entonces es empate
-    return boardToCheck.every(element=>element!==null)
-  }
 
   const updateBoard = (index) => {
     if (board[index] || winner) return;
@@ -52,6 +25,7 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    saveGameToStorage({ board: newBoard, turn: newTurn });
 
     //Verificamos si hay ganador
     const newWinner = checkWinner(newBoard);
@@ -59,7 +33,7 @@ function App() {
     if (newWinner) {
       setWinner(newWinner);
       confetti();
-    }else if(checkEndGame(newBoard)){
+    } else if (checkEndGame(newBoard)) {
       setWinner(false);
     }
   };
@@ -68,6 +42,8 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    resetGameStorage();
   };
 
   return (
@@ -88,14 +64,7 @@ function App() {
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
-      {winner !== null && (
-        <section className="winner">
-          <h2>
-            {winner === false ? "Empate" : `Ganó ${winner}`}
-            <button onClick={reiniciar}>Reiniciar</button>
-          </h2>
-        </section>
-      )}
+      <Winner winner={winner} reiniciar={reiniciar} />
     </main>
   );
 }
