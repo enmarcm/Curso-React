8a9207c5d2836dc64acc8330a58d1dd2c97ba5a3
enmarcm <theenmanuel123@gmail.com>
2023-10-22 01:09:26 -0400

    Tercer proyecto basico de useEffect

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,17 +1,58 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-function App() {
-  const [state, setState] = useState('') 
+const FollowMouse = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [state, setState] = useState("");
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      // console.log(`Posicion actual X:${clientX} Y:${clientY}`);
+      setPosition({ x: clientX, y: clientY });
+    };
 
-  const textStatus = state ? 'Desactivar' : 'Activar'
-  const handlerStatus = () => setState(!state)
+    if (state) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    return () => {
+      console.log('Me desmonte jaja') //<-- Esto se ejecuta cuando el componente se desmonta
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [state]);
+
+  const textStatus = state ? "Desactivar" : "Activar";
+  const handlerStatus = () => setState(!state);
 
   return (
     <>
-      <h3>Proyecto 3</h3>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#111",
+          borderRadius: "50%",
+          opacity: 0.9,
+          border: "1 solid #fff",
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
       <button onClick={handlerStatus}>{textStatus} seguir puntero</button>
     </>
-  )
+  );
+};
+
+function App() {
+  const [montado, setMontado] = useState(false)
+  return (
+    <>
+      {montado && <FollowMouse />}
+      <button onClick={()=>setMontado(!montado)}>Cambiar</button>
+    </>
+  );
 }
 
-export default App
+export default App;
