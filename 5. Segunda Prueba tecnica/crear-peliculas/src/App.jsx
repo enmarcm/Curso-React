8705ac2eaaf2496ae6b6ceea235e7d7da5683164
enmarcm <theenmanuel123@gmail.com>
2024-01-01 15:56:47 -0400

    finalizacion prueba

--- 5. Segunda Prueba tecnica/crear-peliculas/src/App.jsx
+++ 5. Segunda Prueba tecnica/crear-peliculas/src/App.jsx
@@ -1,26 +1,37 @@
 import "./App.css";
-import { useRef, useState, useEffect} from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 import useMovies from "./hooks/useMovies";
 import useSearch from "./hooks/useSearch";
 import { Movies } from "./components/movies";
-
+import debounce from "just-debounce-it";
 
 function App() {
-  const inputRef = useRef()
-  const {search, updateSearch, error} = useSearch()
-  const { movies, getMovies } = useMovies({search});
+  const [sort, setSort] = useState(false);
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    console.log({search})
-    getMovies()
-  }
+  const inputRef = useRef();
+  const { search, updateSearch, error } = useSearch();
+  const { movies, getMovies, loading } = useMovies({ search, sort });
 
-  const handleChange = e => {
-    const newValue = e.target.value
-    if(newValue === ' ') return
-    updateSearch(newValue)
-  }
+  const debounceChangeMovies = useCallback(
+    debounce(({ search }) => {
+      getMovies({ search });
+    }, 300),
+    []
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMovies({ search });
+  };
+
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+    if (newValue === " ") return;
+    updateSearch(newValue);
+    debounceChangeMovies({ search: newValue });
+  };
+
+  const handleCheck = (e) => setSort(!sort);
 
   return (
     <>
@@ -28,15 +39,20 @@ function App() {
         <h1>Buscador de peliculas</h1>
         {search && <p>{search}</p>}
         <form className="form" onSubmit={handleSubmit}>
-          <input name='valor' type="text" placeholder="Avengers, Star Wars, The Matrix..." value={search} onChange={handleChange}/>
+          <input
+            name="valor"
+            type="text"
+            placeholder="Avengers, Star Wars, The Matrix..."
+            value={search}
+            onChange={handleChange}
+          />
+          <input type="checkbox" value={sort} onClick={handleCheck} />
           <button>Buscar</button>
         </form>
-        {error && <p style={{color: 'red'}}>{error}</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
-      <main>
-        <Movies movies={movies} />
-      </main>
+      <main>{loading ? <p>Cargando...</p> : <Movies movies={movies} />}</main>
     </>
   );
 }
--- 5. Segunda Prueba tecnica/crear-peliculas/src/hooks/useMovies.js
+++ 5. Segunda Prueba tecnica/crear-peliculas/src/hooks/useMovies.js
@@ -1,17 +1,35 @@
-import { useState } from "react";
+import { useState, useRef, useMemo, useCallback } from "react";
 import searchMovies from "../services/searchMovies";
 
-const useMovies = ({ search }) => {
+const useMovies = ({ search, sort }) => {
   const [movies, setMovies] = useState([]);
-  
-  const getMovies = async () => {
-    if (search) {
-      const newMovies = await searchMovies({search})
-      setMovies(newMovies)
-    }
-  };
+  const [loading, setLoading] = useState(false);
+  const [errorMovies, setErrorMovies] = useState(null);
+  const previousSearch = useRef(search);
 
-  return { movies, getMovies };
+  const getMovies = useCallback(async ({ search }) => {
+    if (search === previousSearch.current) return;
+
+    try {
+      setErrorMovies(null);
+      setLoading(true);
+      previousSearch.current = search;
+      const newMovies = await searchMovies({ search });
+      setMovies(newMovies);
+    } catch (error) {
+      setErrorMovies(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
+  }, [sort, movies]);
+
+  return { movies: sortedMovies, getMovies, errorMovies, loading };
 };
 
 export default useMovies;
